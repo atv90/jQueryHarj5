f071a3f3bdb1c72db7335a3a692921003b6d382f
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-04-17 09:26:00 +0300

    lisättiin css:ään uusi luokka ja käytettiin sitä js koodissa

--- jQueryHarj5/oma.js
+++ jQueryHarj5/oma.js
@@ -7,18 +7,18 @@ $(document).ready(function () {
         //piilotetaan alueet klikatessa
         $('.content').hide();
         //n‰ytet‰‰n haluttu alue klikatessa
-        $('#one').show('slow');
+        $('#one').show('slow').addClass('laatikko');
     });
     $('#kaks').on('click', function () {
         //piilotetaan alueet klikatessa
         $('.content').hide();
         //n‰ytet‰‰n haluttu alue klikatessa
-        $('#two').show('normal');
+        $('#two').show('normal').addClass('laatikko');
     });
     $('#kol').on('click', function () {
         //piilotetaan alueet klikatessa
         $('.content').hide();
         //n‰ytet‰‰n haluttu alue klikatessa
-        $('#three').show('fast');
+        $('#three').show('fast').addClass('laatikko');
     });
 });
